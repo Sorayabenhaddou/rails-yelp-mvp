877e5c550b6880cb1a1c34cf6612bc380c3906b7
Sorayabenhaddou <soraya-b55@hotmail.fr>
2020-05-13 17:26:14 +0200

    exo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,27 @@
 class ReviewsController < ApplicationController
-def new
+
+  def index
+    @reviews = Review.all
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+   # needed to instantiate the form_for
+  end
+
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -10,10 +29,24 @@ def new
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "new"
     end
   end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to review_path(@review)
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
   private
+
   def review_params
     params.require(:review).permit(:content, :rating)
   end
